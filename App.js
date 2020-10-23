c0fa605e8013f38fa82bdcc67e099e6f81b79bfd
Faith Culas <faithculas@Faiths-MacBook-Pro.local>
2020-10-23 11:09:17 +0530

    scanner 4th commit

--- App.js
+++ App.js
@@ -3,105 +3,64 @@
 import React, {Component} from 'react';
 import {Text, TouchableOpacity, SafeAreaView, Platform} from 'react-native';
 import NfcManager, {Ndef, NfcEvents, NfcTech} from 'react-native-nfc-manager';
+import {NfcRfidScanner} from 'react-native-rfid-nfc-scanner';
 
-function buildUrlPayload(valueToWrite) {
-  //return Ndef.encodeMessage([Ndef.uriRecord(valueToWrite)]);
-  return Ndef.encodeMessage([Ndef.textRecord(valueToWrite)]);
+export default function App() {
+  const scanner = new NfcRfidScanner();
+
+  const initiate = () => {
+    scanner.init();
+    console.log('scanner is initiated');
+  };
+
+  const isEnabled = () => {
+    scanner.isEnabled();
+    console.log('scanner is enabled');
+  };
+
+  const getStatus = () => {
+    let status = scanner.getStatus();
+    console.log('status is ', status);
+  };
+
+  return (
+    <SafeAreaView style={{padding: 20}}>
+      <Text>NFC SCANNER</Text>
+      <TouchableOpacity
+        style={{
+          padding: 10,
+          width: 200,
+          margin: 20,
+          borderWidth: 1,
+          borderColor: 'black',
+        }}
+        onPress={initiate}>
+        <Text>Initiate Scanner</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          padding: 10,
+          width: 200,
+          margin: 20,
+          borderWidth: 1,
+          borderColor: 'black',
+        }}
+        onPress={isEnabled}>
+        <Text>is Enabled ? </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          padding: 10,
+          width: 200,
+          margin: 20,
+          borderWidth: 1,
+          borderColor: 'black',
+        }}
+        onPress={getStatus}>
+        <Text>Get Status</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
-
-class App extends Component {
-  componentDidMount() {
-    NfcManager.start();
-    /* console.log('started');
-    NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
-      console.warn('tag', tag);
-      NfcManager.unregisterTagEvent().catch(() => 0);
-    }); */
-  }
-
-  componentWillUnmount() {
-    NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
-    NfcManager.unregisterTagEvent().catch(() => 0);
-  }
-
-  render() {
-    return (
-      <SafeAreaView style={{padding: 20}}>
-        <Text>NFC Demo</Text>
-        <TouchableOpacity
-          style={{
-            padding: 10,
-            width: 200,
-            margin: 20,
-            borderWidth: 1,
-            borderColor: 'black',
-          }}
-          onPress={this._testNdef}>
-          <Text>Test Ndef</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={{
-            padding: 10,
-            width: 200,
-            margin: 20,
-            borderWidth: 1,
-            borderColor: 'black',
-          }}
-          onPress={this._cleanUp}>
-          <Text>Cancel Test</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={{
-            padding: 10,
-            width: 200,
-            margin: 20,
-            borderWidth: 1,
-            borderColor: 'black',
-          }}
-          onPress={this.checkSupport}>
-          <Text>Check Support</Text>
-        </TouchableOpacity>
-      </SafeAreaView>
-    );
-  }
-
-  _cleanUp = () => {
-    NfcManager.unregisterTagEvent().catch(() => 0);
-  };
-
-  _testNdef = async () => {
-    try {
-      let tech = Platform.OS === 'ios' ? NfcTech.MifareIOS : NfcTech.NfcA;
-      let resp = await NfcManager.requestTechnology(tech, {
-        alertMessage: 'Ready to do some custom Mifare cmd!',
-      });
-      console.warn(resp);
-
-      // the NFC uid can be found in tag.id
-      let tag = await NfcManager.getTag();
-      console.warn(tag);
-
-      if (Platform.OS === 'ios') {
-        resp = await NfcManager.sendMifareCommandIOS([0x30, 0x00]);
-      } else {
-        resp = await NfcManager.transceive([0x30, 0x00]);
-      }
-      console.warn(resp);
-
-      this._cleanUp();
-    } catch (ex) {
-      console.warn('ex', ex);
-      this._cleanUp();
-    }
-  };
-
-  checkSupport = () => {
-    NfcManager.isSupported(NfcTech.MifareClassic)
-      .then(() => console.log('Mifare classic is supported'))
-      .catch((err) => console.warn(err));
-  };
-}
-
-export default App;
